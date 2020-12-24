a5d42b9e9390d070af78726864dbfb2390290348
Qi Li <liqihao2000@126.com>
2020-12-24 09:53:55 +0800

    first example

---
+++ example/Delta.jl
@@ -0,0 +1,10 @@
+println("--------绘制*的三角形------------")
+for i in range(1,stop=3)
+    for j in range(1,stop=3-i)
+        print(" ")
+    end
+    for k in range(1,stop=2*i-1)
+        print("*")
+    end
+    print("\n")
+end
